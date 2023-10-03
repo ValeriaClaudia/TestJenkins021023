8c9d7438723be3bbf17fc67171cf2c6158895053
Valeria Claudia <valeriaguglielmetti@gmail.com>
2023-10-02 21:50:24 -0300

    first commit

---
+++ src/main/java/com/mycompany/testjenkins/TestJenkins.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testjenkins;
+
+/**
+ *
+ * @author Valeria
+ */
+public class TestJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
